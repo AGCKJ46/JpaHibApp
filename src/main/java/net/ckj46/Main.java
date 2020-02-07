d78179f45828bef0c1c8b4e143deccc3a2cbb1e4
AGruszczewski <agruszczewski@post.pl>
2020-02-07 15:15:09 +0100

    L07

--- src/main/java/net/ckj46/Main.java
+++ src/main/java/net/ckj46/Main.java
@@ -1,13 +1,25 @@
 package net.ckj46;
 
+import net.ckj46.domain.Employee;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class Main {
     public static void main (String[] args){
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("h2db");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mysql");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Employee employee = new Employee();
+        employee.setFirstName("Jarosław");
+        employee.setLastName("Dupa-Kaczyński");
+        employee.setSalary(1L);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(employee);
+        entityManager.getTransaction().commit();
 
         entityManager.close();
         entityManagerFactory.close();
---
+++ src/main/java/net/ckj46/domain/Employee.java
@@ -0,0 +1,49 @@
+package net.ckj46.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private Long salary;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
+}
