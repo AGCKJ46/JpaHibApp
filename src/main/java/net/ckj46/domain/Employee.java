329f1ed30b0f053d3e016535c7312711f9b9870c
AGruszczewski <agruszczewski@post.pl>
2020-02-19 10:11:25 +0100

    L08

--- src/main/java/net/ckj46/domain/Employee.java
+++ src/main/java/net/ckj46/domain/Employee.java
@@ -1,18 +1,22 @@
 package net.ckj46.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="Employees")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name="employeeId")
     private Long id;
 
+    @Column(nullable = false, length=30)
     private String firstName;
+
+    @Column(columnDefinition = "VARCHAR(70) NOT NULL")
     private String lastName;
+
+    @Column(nullable = false, precision = 5, scale = 0)
     private Long salary;
 
     public long getId() {
